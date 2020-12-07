42d0819613ba581f90dc0dfa65fb4e10c302a9a6
CitraCamila <citracamila@gmail.com>
2020-12-07 19:34:23 +0700

    Commit FinalProjectMobile pertama

---
+++ Scripts/Create New Account/Script1607343944845.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('D:\\Final Project 2\\FINAL PROJECTS\\MOBILE\\app-debug.apk', true)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText - Username (1)'), 'citracamila', 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText - Password (1)'), 'Telco@2014', 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.CheckBox - Remember Credentials'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - LOGIN'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - ADD ACCOUNT'), 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText - Account Name'), 'Mona', 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText - Initial Balance (Optional)'), '5000000', 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - ADD'), 0)
+
+Mobile.closeApplication()
+
